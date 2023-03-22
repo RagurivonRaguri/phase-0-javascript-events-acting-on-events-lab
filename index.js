59f9eedbafc593470111c5bc1ac62f882a96c7e6
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-22 18:40:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+// // Your code here
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  function moveDodgerRight() {
+    const RightNumbers = dodger.style.left.replace("px", "");
+    const way = parseInt(RightNumbers, 10);
+  
+    if (way > 0) {
+      dodger.style.left = `${way + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
